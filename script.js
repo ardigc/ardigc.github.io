f542b85cb84ac75094c378179c9ae6ae363a77bc
JuanMOlivas <41582680+CerealPlayer@users.noreply.github.com>
2023-04-13 10:44:17 +0000

    Add promises

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("Hola")
+// Con event loop
+
+fetch("http://swapi.dev/api/starships/", {
+  method: "GET",
+})
+  .then((res) => {
+    return res.json();
+  })
+  .then((body) => {
+    console.log(body);
+  });
+
+// Deteniendo ejecución
+
+// async function getLuke() {
+//   const response = await fetch("http://swapi.dev/api/people/1/", {
+//     method: "GET",
+//   });
+//   const body = await response.json();
+//   return body;
+// }
+
+// async function main() {
+//   const luke = await getLuke()
+//   console.log(luke)
+// }
+
+// main()
