11adfcfd3b44504087b2f0ec5a772428c42b0f96
ardigc <130089987+ardigc@users.noreply.github.com>
2023-04-17 19:53:36 +0000

    Casi terminado people

--- people/script.js
+++ people/script.js
@@ -1,7 +1,10 @@
 const search = new URLSearchParams(window.location.search);
 // const url = window.location.href
 const id = search.get("id");
-
+const intro =document.querySelector(".intro");
+intro.addEventListener("click", (ev) => {
+  window.location.assign(window.location.origin+window.location.pathname)
+});
 if (!id === !null) {
   // Con event loop
   fetch("https://swapi.dev/api/people/", {
@@ -41,6 +44,7 @@ if (!id === !null) {
     const massBox = createElement("div", "item-box", "Peso: " + person.mass + " Kg")
     const hairBox = createElement("div", "item-box", "Color de pelo: " + person.hair_color)
     const skinBox = createElement("div", "item-box", "Color de piel: " + person.skin_color)
+    const birthBox = createElement("div", "item-box", "Año de nacimiento: " + person.birth_year)
     personBox.append(nameBox);
     personBox.append(eyeBox);
     personBox.append(genderBox);
@@ -48,10 +52,11 @@ if (!id === !null) {
     personBox.append(massBox);
     personBox.append(hairBox);
     personBox.append(skinBox);
+    personBox.append(birthBox);
     const ident = person.url;
     var regex = /(\d+)/g;
     const ide = ident.match(regex);
-    console.log(person);
+    // console.log(person);
     personBox.addEventListener("click", (ev) => {
       window.location.assign(window.location.href + "?id=" + ide)
     });
@@ -79,8 +84,11 @@ if (!id === !null) {
         const massBox = createElement("div", "item-box", "Peso: " + person.mass + " Kg")
         const hairBox = createElement("div", "item-box", "Color de pelo: " + person.hair_color)
         const skinBox = createElement("div", "item-box", "Color de piel: " + person.skin_color)
+        const birthBox = createElement("div", "item-box", "Año de nacimiento: " + person.birth_year)
         const shipBox = createShipBox(person.starships);
         const filmBox = createFilmBox(person.films);
+        const vehiclesBox = createVehiclesBox(person.vehicles);
+        const planetBox = createPlanetBox(person.homeworld);
         personBox.append(nameBox);
         personBox.append(eyeBox);
         personBox.append(genderBox);
@@ -90,6 +98,9 @@ if (!id === !null) {
         personBox.append(hairBox);
         personBox.append(skinBox);
         personBox.append(filmBox);
+        personBox.append(vehiclesBox);
+        personBox.append(planetBox);
+        personBox.append(birthBox);
         return personBox;
       }
 
@@ -107,7 +118,7 @@ function createShipBox(link) {
   } else {
     const shipBox = createElement("div", "item-box",)
     const textBox = createElement("div", "item-box", "Naves pilotadas: ")
-    console.log(link)
+    // console.log(link)
     const shipList = link.map(createShipList);
     shipBox.append(textBox);
     shipBox.append(...shipList);
@@ -123,16 +134,48 @@ function createFilmBox(link) {
   } else {
     const filmBox = createElement("div", "item-box",)
     const textBox = createElement("div", "item-box", "Aparece en: ")
-    console.log(link)
+    // console.log(link)
     const filmList = link.map(createFilmList);
     filmBox.append(textBox);
     filmBox.append(...filmList);
     return filmBox;
   }
 }
+function createVehiclesBox(link) {
+  if (link.length === 0) {
+    const vehiclesBox = createElement("div", "item-box",)
+    const textBox = createElement("div", "item-box", "Vehiculos conocidos: ninguno")
+    vehiclesBox.append(textBox);
+    return vehiclesBox;
+  } else {
+    const vehiclesBox = createElement("div", "item-box",)
+    const textBox = createElement("div", "item-box", "Vehiculos conocidos: ")
+    // console.log(link)
+    const vehiclesList = link.map(createVehiclesList);
+    vehiclesBox.append(textBox);
+    vehiclesBox.append(...vehiclesList);
+    return vehiclesBox;
+  }
+}
+function createPlanetBox(link) {
+  if (link.length === 0) {
+    const planetBox = createElement("div", "item-box",)
+    const textBox = createElement("div", "item-box", "Planeta de origen desconocido")
+    planetBox.append(textBox);
+    return planetBox;
+  } else {
+    const planetBox = createElement("div", "item-box",)
+    const textBox = createElement("div", "item-box", "Planeta de origen: ")
+    // console.log(link)
+    const planetList = createPlanetList(link);
+    planetBox.append(textBox);
+    planetBox.append(planetList);
+    return planetBox;
+  }
+}
 function createShipList(link) {
   const ship = createElement("div", "item-box",)
-  console.log(ship)
+  // console.log(ship)
   fetch(link, {
     method: "GET",
   })
@@ -150,7 +193,7 @@ function createShipList(link) {
 }
 function createFilmList(link) {
   const film = createElement("div", "item-box",)
-  console.log(film)
+  // console.log(film)
   fetch(link, {
     method: "GET",
   })
@@ -166,6 +209,42 @@ function createFilmList(link) {
 
   return film
 }
+function createVehiclesList(link) {
+  const vehicles = createElement("div", "item-box",)
+  // console.log(vehicles)
+  fetch(link, {
+    method: "GET",
+  })
+    .then((res) => {
+      return res.json();
+    }).then((name) => {
+      const vehiclesName = createElement("a", "link-box", name.name)
+      const vehiclesUrlLink = linkUrl(link);
+      const finalLink = linkVehicles(vehiclesUrlLink)
+      vehiclesName.setAttribute("href", finalLink)
+      vehicles.append(vehiclesName)
+    })
+
+  return vehicles
+}
+function createPlanetList(link) {
+  const planet = createElement("div", "item-box",)
+  // console.log(planet)
+  fetch(link, {
+    method: "GET",
+  })
+    .then((res) => {
+      return res.json();
+    }).then((name) => {
+      const planetName = createElement("a", "link-box", name.name)
+      const planetUrlLink = linkUrl(link);
+      const finalLink = linkPlanet(planetUrlLink)
+      planetName.setAttribute("href", finalLink)
+      planet.append(planetName)
+    })
+
+  return planet
+}
 function linkUrl(url) {
   var regex = /(\d+)/g;
   const ide = url.match(regex);
@@ -177,6 +256,14 @@ function linkShip(num) {
 }
 function linkFilm(num) {
   const link = window.location.origin + "/films/?id=" + num;
+  return link
+}
+function linkVehicles(num) {
+  const link = window.location.origin + "/vehicles/?id=" + num;
+  return link
+}
+function linkPlanet(num) {
+  const link = window.location.origin + "/planets/?id=" + num;
   return link
 }
 function createElement(tag, styles, content) {
